6d4a522d6ee86b1620f0935375bc3087fd9d0d3c
georgiana <someone@someplace.com>
2021-05-19 11:12:24 +0300

    some init msg

---
+++ power/power.go
@@ -0,0 +1,7 @@
+package power
+
+import "math"
+
+func CalculatePower(n int, p int) int {
+	return math.Pow(n, p)
+}
---
+++ sum/sum.go
@@ -0,0 +1,10 @@
+package sum
+
+func CalculateSum(n ...int) int {
+	sum := 0
+	for _, v := range n {
+		sum += v
+	}
+
+	return sum
+}
